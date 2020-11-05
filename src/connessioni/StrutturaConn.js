b17abd051f2e29d1ffec8d4bd409c53539897900
Mikednv <35602963+Mikednv@users.noreply.github.com>
2020-11-05 10:17:36 +0100

    Update StrutturaConn.js

--- src/connessioni/StrutturaConn.js
+++ src/connessioni/StrutturaConn.js
@@ -3,58 +3,58 @@ import axios from 'axios';
 
 class StrutturaConn {
     registraStruttura = (obj) => {
-        return axios.post('http://localhost:3006/strutture/strutture', obj)
+        return axios.post('https://progettomdmc.herokuapp.com/strutture/strutture', obj)
     }
     modificaStruttura = (obj) => {
-        return axios.post('http://localhost:3006/strutture/modificaStruttura', obj)
+        return axios.post('https://progettomdmc.herokuapp.com/strutture/modificaStruttura', obj)
     }
     registraCamera = (obj) => {
-        return axios.post('http://localhost:3006/strutture/registraCamera', obj)
+        return axios.post('https://progettomdmc.herokuapp.com/strutture/registraCamera', obj)
     }
     modificaCamera = (obj) => {
-        return axios.post('http://localhost:3006/strutture/modificaCamera', obj)
+        return axios.post('https://progettomdmc.herokuapp.com/strutture/modificaCamera', obj)
     }
 
     aggiungiFoto = (dati) => {
-        return axios.post('http://localhost:3006/strutture/immagini', dati)
+        return axios.post('https://progettomdmc.herokuapp.com/strutture/immagini', dati)
     }
 
     aggiungiImmaginiCamera = (dati) => {
-        return axios.post('http://localhost:3006/strutture/immaginiCamera', dati)
+        return axios.post('https://progettomdmc.herokuapp.com/strutture/immaginiCamera', dati)
     }
 
     getStrutture = (id) => {
-        return axios.post('http://localhost:3006/strutture/getStrutture',{id,})
+        return axios.post('https://progettomdmc.herokuapp.com/strutture/getStrutture',{id,})
 
     }
     getCamere = (id) => {
-        return axios.post('http://localhost:3006/strutture/getCamere',{id,})
+        return axios.post('https://progettomdmc.herokuapp.com/strutture/getCamere',{id,})
 
     }
     getNome = () => {
-        return axios.get('http://localhost:3006/strutture/getNome')
+        return axios.get('https://progettomdmc.herokuapp.com/strutture/getNome')
 
     }
     deleteStruttura = (id_struttura) => {
-        return axios.post('http://localhost:3006/strutture/deleteStruttura',{id_struttura})
+        return axios.post('https://progettomdmc.herokuapp.com/strutture/deleteStruttura',{id_struttura})
 
     }
     cancellaCamera = (id_camera) => {
-        return axios.post('http://localhost:3006/strutture/cancellaCamera',{id_camera})
+        return axios.post('https://progettomdmc.herokuapp.com/strutture/cancellaCamera',{id_camera})
 
     }
     getTassa = (id_gestore) => {
-        return axios.post('http://localhost:3006/strutture/getTassa',{id_gestore,})
+        return axios.post('https://progettomdmc.herokuapp.com/strutture/getTassa',{id_gestore,})
 
     }
     prendiImmagini = (id)=>{
-        return axios.post('http://localhost:3006/strutture/fetchImage', {id,})
+        return axios.post('https://progettomdmc.herokuapp.com/strutture/fetchImage', {id,})
     }
     modificaFoto = (dati) => {
-        return axios.post('http://localhost:3006/strutture/modificaFoto', dati)
+        return axios.post('https://progettomdmc.herokuapp.com/strutture/modificaFoto', dati)
     }
     cancella = (obj) => {
-        return axios.post('http://localhost:3006/strutture/cancella', obj)
+        return axios.post('https://progettomdmc.herokuapp.com/strutture/cancella', obj)
     }
 
 }
