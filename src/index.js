7b7518723ae16bb2aa1887ebe12a79faec64415b
Mikednv <35602963+Mikednv@users.noreply.github.com>
2020-11-05 06:13:21 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+const res = await fetch('https://https://objective-edison-48fce5.netlify.app/')
 
 
 ReactDOM.render(
