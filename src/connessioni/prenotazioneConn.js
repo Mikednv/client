fb81ac229ae15752824d67a57921b771f072d08e
Mikednv <35602963+Mikednv@users.noreply.github.com>
2020-11-05 10:18:40 +0100

    Update prenotazioneConn.js

--- src/connessioni/prenotazioneConn.js
+++ src/connessioni/prenotazioneConn.js
@@ -3,35 +3,35 @@ import axios from 'axios';
 
      class prenotazioneConn {
         pagamento = (obj) => {
-            return axios.post("http://localhost:3006/" + "prenotazione/pagamento", obj);
+            return axios.post("https://progettomdmc.herokuapp.com/" + "prenotazione/pagamento", obj);
             }
         prenotaCamera = (obj) => {
-            return axios.post("http://localhost:3006/" + "strutture/prenotaCamera", obj);
+            return axios.post("https://progettomdmc.herokuapp.com/" + "strutture/prenotaCamera", obj);
             }
         prenotaStruttura = (obj) => {
-            return axios.post("http://localhost:3006/" + "prenotazione/prenotaStruttura", obj);
+            return axios.post("https://progettomdmc.herokuapp.com/" + "prenotazione/prenotaStruttura", obj);
             }    
         getid = (obj) => {
-            return axios.post('http://localhost:3006/prenotazione/getId', obj); }
+            return axios.post('https://progettomdmc.herokuapp.com/prenotazione/getId', obj); }
        
         conferma = (obj) => {
-            return axios.post('http://localhost:3006/prenotazione/conferma', obj); }
+            return axios.post('https://progettomdmc.herokuapp.com/prenotazione/conferma', obj); }
         
         getGuadagni = (id) => {
-            return axios.post('http://localhost:3006/prenotazione/getGuadagni',{id,})
+            return axios.post('https://progettomdmc.herokuapp.com/prenotazione/getGuadagni',{id,})
     
         }
         getPrenotazioniStrutture = (id) => {
-            return axios.post('http://localhost:3006/prenotazione/getPrenotazioniStrutture', {id,});
+            return axios.post('https://progettomdmc.herokuapp.com/prenotazione/getPrenotazioniStrutture', {id,});
         }
         getPrenotazioniCamere = (id) => {
-            return axios.post('http://localhost:3006/prenotazione/getPrenotazioniCamere', {id,});
+            return axios.post('https://progettomdmc.herokuapp.com/prenotazione/getPrenotazioniCamere', {id,});
         }
         getPrenotazioniUtenteStrutture = (id) => {
-            return axios.post('http://localhost:3006/prenotazione/getPrenotazioniUtenteStrutture', {id,});
+            return axios.post('https://progettomdmc.herokuapp.com/prenotazione/getPrenotazioniUtenteStrutture', {id,});
         }
         getPrenotazioniUtenteCamere = (id) => {
-            return axios.post('http://localhost:3006/prenotazione/getPrenotazioniUtenteCamere', {id,});
+            return axios.post('https://progettomdmc.herokuapp.com/prenotazione/getPrenotazioniUtenteCamere', {id,});
         }
     }   
 
