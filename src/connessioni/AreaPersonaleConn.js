91ed43b7c4d3c9020eda25f2bfa9cd80d86acc20
Mikednv <35602963+Mikednv@users.noreply.github.com>
2020-11-05 10:15:16 +0100

    Update AreaPersonaleConn.js

--- src/connessioni/AreaPersonaleConn.js
+++ src/connessioni/AreaPersonaleConn.js
@@ -3,16 +3,16 @@ import axios from 'axios';
 
      class AreaPersonaleConn {
     fetchUtente=(id)=>{
-            return axios.post("http://localhost:3006/"+ "areaPersonale/fetchUtenti", {id,});
+            return axios.post("https://progettomdmc.herokuapp.com/"+ "areaPersonale/fetchUtenti", {id,});
     }
     modificaDati = (obj) => {
-    return axios.post("http://localhost:3006/" + "areaPersonale/modificaDati", obj);
+    return axios.post("https://progettomdmc.herokuapp.com/" + "areaPersonale/modificaDati", obj);
     }
     modificaPassword = (obj)=>{
-        return axios.post("http://localhost:3006/"+ "areaPersonale/modificaPassword", obj);
+        return axios.post("https://progettomdmc.herokuapp.com/"+ "areaPersonale/modificaPassword", obj);
     }
     getGuadagni=()=>{
-        return axios.get("http://localhost:3006/"+ "areaPersonale/getGuadagni");
+        return axios.get("https://progettomdmc.herokuapp.com/"+ "areaPersonale/getGuadagni");
     }
 
 }
